6a9218f6d815f6bc1b695777a44cef419e02d5a1
reginpv <reginpv@gmail.com>
2021-11-19 07:44:21 +0800

    popup menu

--- components/popupMainMenu.js
+++ components/popupMainMenu.js
@@ -1,11 +1,28 @@
 //
-import MainMenu from "./mainmenu";
+import { useDispatch } from "react-redux";
+import { setPopupMenu } from "../store/uiSlice";
 
-export default function PopupMainmene({ className }) {
+//
+import Logo from "./logo";
+import MainMenu from "./mainmenu";
+import { IconClose } from "./templates/icons";
+
+//
+
+
+export default function PopupMainmenu({ className }) {
+
+  const dispatch = useDispatch()
 
   return (
-    <section className={`${className}`}>
-      <MainMenu />
+    <section className={`w-screen h-screen fixed top-0 left-0 bg-white flex flex-col space-y-5 ${className}`}>
+      <div className="flex items-center justify-between h-header border-b px-5">
+        <Logo />
+        <button onClick={()=>dispatch(setPopupMenu(false))}>
+          <IconClose />
+        </button>
+      </div>
+      <MainMenu className="flex-col space-x-0 space-y-5 px-5" />
     </section>
   )
 }
--- components/templates/layoutDefault.js
+++ components/templates/layoutDefault.js
@@ -1,13 +1,23 @@
+//
+import { useSelector } from "react-redux"
+
 // Components
 import Header from "../header"
 import Footer from "../footer"
+import PopupMainmenu from "../popupMainMenu"
 
 export default function LayoutDefault({ className, children }) {
+
+  const { popupMenu } = useSelector(state => state.ui)
+
   return (
     <section className={`${className} font-karla text-body flex flex-col min-h-screen`}>
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
+      {
+        popupMenu ? <PopupMainmenu /> : null
+      }
     </section>
   )
 }
