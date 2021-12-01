34d7f830c64233e01f056463ec7103fd8a8abe4b
= <gvaleriano@pickup.ph>
2021-12-02 00:10:39 +0800

    fix: header button signup

--- components/header.js
+++ components/header.js
@@ -1,3 +1,6 @@
+//
+import Link from 'next/link'
+
 // Redux
 import { useDispatch, useSelector } from "react-redux"
 import { setPopupMenu } from "../store/uiSlice";
@@ -5,7 +8,6 @@ import { setPopupMenu } from "../store/uiSlice";
 // Components
 import Logo from "./logo";
 import MainMenu from "./mainmenu";
-import Button from './templates/button'
 import { IconMenu } from "./templates/icons";
 
 export default function Header() {
@@ -21,13 +23,12 @@ export default function Header() {
       </div>
       <div className="flex items-center space-x-5">
         <a href="https://app.pickup.ph" target="_blank" rel="noopener noreferrer" className="text-fs-button uppercase px-5 py-2 font-semibold hidden sm:block">Login</a>
-        <Button 
-          type="link" 
-          link="/signup"
-          className="md:text-[14px] uppercase bg-primary1 py-[8px] px-[20px] rounded-md font-semibold whitespace-nowrap transition duration-100 ease-in-out hover:brightness-90"
-        >
-          Sign up free
-        </Button>
+        <Link href="/signup">
+          <a className={`text-[14px] uppercase bg-primary1 py-[8px] px-[20px] rounded-md font-semibold whitespace-nowrap transition duration-100 ease-in-out hover:brightness-90`}
+          >
+            Sign up free
+          </a>
+        </Link>
         <button onClick={()=>dispatch(setPopupMenu(popupMenu ? false : true))}>
           <IconMenu className="block header:hidden" />
         </button>
