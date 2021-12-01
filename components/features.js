e27c02f024e99c30c2fc0e8eb29bc3c9a2590bab
= <gvaleriano@pickup.ph>
2021-12-02 02:01:53 +0800

    features update

--- components/features.js
+++ components/features.js
@@ -9,13 +9,13 @@ export default function Features({ className }) {
   return (
     <section className={`${className}`}>
       <div className="py-20 px-5 container mx-auto">
-        <div className="grid md:grid-cols-3 gap-5 mb-20">
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-5 mb-20">
           {
             dataFeatures.map((f,i)=>(
               <div key={i} className="flex justify-center mb-5 md:mb-0">
                 <div className="md:max-w-narrowXS">
-                  <div className="py-10 md:py-0 flex items-center justify-center bg-gray-100 min-h-[310px] rounded-2xl md:h-imageBoxMD lg:h-imageBox">
-                    <Image src={f.image.src} width={64} height={64} alt={f.label} />
+                  <div className="py-10 md:py-0 flex items-center justify-center rounded-2xl md:h-imageBoxMD lg:h-imageBox">
+                    <Image src={f.image.src} width={310} height={310} alt={f.label} className="shadow hover:shadow-md" />
                   </div>
                   <div>
                     <h3 className="text-fs-boxSubHeader text-center my-5">{f.label}</h3>
@@ -29,7 +29,7 @@ export default function Features({ className }) {
           }
         </div>
         <div className="text-center">
-          <Button bg="bg-white" className=" border border-primary2 text-primary2 hover:bg-primary2 hover:text-white">See All Features</Button>
+          <Button type="link" link="/pricing" bg="bg-white" className=" border border-primary2 text-primary2 hover:bg-primary2 hover:text-white">See All Features</Button>
         </div>
       </div>
     </section>
