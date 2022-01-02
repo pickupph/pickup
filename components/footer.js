37c6819c8cd1b0c7331f06e88628ac6539312763
gvaleriano <gvaleriano@pickup.ph>
2022-01-02 12:58:05 +0800

    full year

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,10 @@
+import { SITE_FULL_YEAR } from "../config/constants";
+
 export default function Footer({ className }) {
   return (
     <footer className="text-footerMenu text-fs-footerMenu p-5 text-center">
       <div className="container mx-auto">
-         <p>Copyright © 2020 Pickup.ph. This site was made with ❤️ in the Philippines.</p>
+         <p>Copyright © {SITE_FULL_YEAR} Pickup.ph. This site was made with ❤️ in the Philippines.</p>
       </div>
     </footer>
   )
--- config/constants.js
+++ config/constants.js
@@ -3,6 +3,7 @@ export const SITE_TITLE = "Pickup.ph - FREE Online Ordering System and Delivery 
 export const SITE_DESCRIPTION = "Start your contactless retail site today to sell online and manage deliveries with ZERO commissions."
 export const SITE_IMAGE_DEFAULT = "https://pickupph.vercel.app/images/image-default.png"
 export const SITE_IMAGE_DEFAULT_REL = "/images/image-default.png"
+export const SITE_FULL_YEAR = new Date().getFullYear()
 
 export const CURRENCY_SYMBOL = "P"
 export const CURRENCY_NAME = "pesos"
