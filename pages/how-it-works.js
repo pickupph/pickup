c3bc7bc78d537e77e2377c149fb17a0d267da03f
gvaleriano <gvaleriano@pickup.ph>
2022-03-06 14:49:21 +0800

    how it works partial

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -1,5 +1,10 @@
+//
+import Image from "next/image"
+
 // Components
 import LayoutDefault from "../components/templates/layoutDefault"
+import Section from "../components/templates/section"
+import Button from "../components/templates/button"
 
 export default function HowItWorks() {
 
@@ -10,10 +15,22 @@ export default function HowItWorks() {
       }}
     >
 
-      <div className="container mx-auto px-5 py-5 md:py-10 text-center">
-        <h1 className="text-[40px] md:text-fs-heroHeader">How it works</h1>
-        <p className="text-fs-body mx-auto max-w-[780px]">Coming soon...</p>
-      </div>
+      <Section>
+        <div className="max-w-[1000px] mx-auto py-10">
+          <div className="grid md:grid-cols-2 items-center gap-10 md:gap-20">
+            <div className="text-center md:text-left">
+              <h1 className="text-[38px] md:text-[40px] lg:text-[52px] font-semibold leading-tight mb-8">The most powerful commission-free mobile ordering</h1>
+              <p className="text-[20px] lg:text-[22px] mb-8">Create an amazing branded customer experience, increase your revenue and margins while owning the customer data</p>
+              <div>
+                <Button type="link" link="/book-a-demo" bg="bg-primary2" className="my-5 h-buttonLg text-white hover:brightness-90">Get Started for Free</Button>
+              </div>
+            </div>
+            <div className="text-center">
+              <Image src="/images/placeholders/how-it-works.jpeg" alt="Pickup how it works" width={500} height={686} className="rounded-[30px]" />
+            </div>
+          </div>
+        </div>
+      </Section>
 
     </LayoutDefault>
     
