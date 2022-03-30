e3d6ab9da412dde55761adcddf35ce51abacc52b
ginvaleriano <gvaleriano@pickup.ph>
2022-03-31 00:27:55 +0800

    udpate title

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -26,7 +26,7 @@ export default function HowItWorks() {
         <div className="max-w-[1000px] mx-auto py-10">
           <div className="grid md:grid-cols-2 items-center gap-10 md:gap-20">
             <div className="text-center md:text-left">
-              <h1 className="text-[38px] md:text-[40px] lg:text-[52px] font-semibold leading-tight mb-8">The most powerful commission-free mobile ordering</h1>
+              <h1 className="text-[38px] md:text-[40px] lg:text-[52px] font-semibold leading-tight mb-8">The most powerful commission-free mobile ordering tool available</h1>
               <p className="text-[20px] lg:text-[22px] mb-8">Create an amazing branded customer experience, increase your revenue and margins while owning the customer data</p>
               <div>
                 <Button type="link" link="/signup" bg="bg-primary2" className="my-5 h-buttonLg text-white hover:brightness-90">Get Started for Free</Button>
